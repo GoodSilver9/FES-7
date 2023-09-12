f0f47a0eaf22d37f760cebb4d2bc2a9d3f2dad64
GoodSilver9 <hool784@naver.com>
2023-09-12 17:34:44 +0900

    09월12일 React

---
+++ 코딩테스트연습/2023/09/덧칠하기/test.js
@@ -0,0 +1,22 @@
+function solution(n, m, section) {
+  let arr = [];
+  let result = [];
+  let count = 0;
+  for (i = 1; i <= n; i++) {
+    arr.push(i);
+  }
+  //[1,2,3,4,5,6,7,8]
+  let newArr = 
+}
+
+//   let painting = arr.slice(item - 1, item + m - 1);
+solution(8, 4, [2, 3, 6]); //result 2
+
+// 그러니까 [1,2,3,4,5,6,7,8] 중에서 2,3,6 을 칠해야되나
+// 길이가 4개밖에 못칠하니까  2 부터 5까지 하고 아근데 뒤에서부터 하는게좋겠다
+// 다음은 6부터 9까지 하겠지? 근데 9는없고
+
+// sort 로 정렬하고? 근데 이미 정렬되있는거같은데 아무튼
+// 0번째 index 부터 ? m 만큼 증가시켜서? slice 로 잘라네고 filter로 나머지를? 출력? 할까
+
+//for 문을 써야될까 cnt 값을 올려야되는데 안써도되나
